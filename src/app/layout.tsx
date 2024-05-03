53002061787ea5675b6f093cfc01d491f1bf2337
Victor <emmyvic98@gmail.com>
2024-05-03 13:01:51 +0100

    favicon change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>{children}</body>
     </html>
   );
