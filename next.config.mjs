28fe33bef9b3cfc34d748f86b80ff13fd8757359
Victor <emmyvic98@gmail.com>
2024-05-03 12:45:10 +0100

    favicon change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+      },
+      output: "export",
+      images: {
+        unoptimized: true,
+      },
+      trailingSlash: true,
+};
 
 export default nextConfig;
