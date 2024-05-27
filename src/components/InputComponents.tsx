923cb58025e007525b8c162c38a4412d38d9794a
Victor <emmyvic98@gmail.com>
2024-05-27 10:32:45 +0100

    Otp Inputs

--- src/components/InputComponents.tsx
+++ src/components/InputComponents.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useRef, useState } from "react";
 import styled from "styled-components";
 
 export const FormSection = styled.div`
@@ -20,4 +21,74 @@ export const StyledTextArea = styled.textarea`
   display: block;
   width: 100%;
   padding: 12.93px 17.25px;
+`;
+
+export const OtpInput = () => {
+  const [inputs, setInputs] = useState<number[]>(Array(4).fill(null));
+  const itemsRef = useRef<Map<number, HTMLInputElement | null> | null>(null);
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    setInputs(
+      inputs.map((el, i) => {
+        if (i === index) {
+          // Extra logic for keeping it to single digits
+          let currentValue = event.currentTarget.value;
+          if (el !== null) {
+            const previousValue = el.toString();
+            currentValue = currentValue.replace(previousValue, "");
+          }
+          return Number(currentValue);
+        }
+        return el;
+      })
+    );
+    nextInputFocus(index);
+  };
+
+  const nextInputFocus = (index: number) => {
+    const map = getMap();
+    if (index < inputs.length - 1) {
+      const node = map.get(index + 1); // select the next form input
+      node?.focus();
+    }
+  };
+  const getMap = () => {
+    if (!itemsRef.current) {
+      itemsRef.current = new Map();
+    }
+    return itemsRef.current;
+  };
+  return (
+    <OtpContainer className="mulish text-center">
+      {inputs.map((el, i) => (
+        <StyledInput
+          type="number"
+          max={9}
+          className="mb-4"
+          key={i}
+          ref={(node) => {
+            const map = getMap();
+            node ? map.set(i, node) : map.delete(i);
+          }}
+          onChange={(event) => handleChange(event, i)}
+          value={el === null ? "" : el.toString()}
+        />
+      ))}
+    </OtpContainer>
+  );
+};
+
+const OtpContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 43px;
+
+  ${StyledInput} {
+    padding: 12.93px 20px;
+    font-size: 29.58px;
+    font-weight: 800;
+  }
 `;
